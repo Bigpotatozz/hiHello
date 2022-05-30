90bd8a2308b3998ad59952429155dcc2d1fc7505
Bigpotatozz <78667961+Bigpotatozz@users.noreply.github.com>
2022-05-29 20:26:38 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+
+    
+
+
+const menuR = $("#menuIcon")
+const opciones = $("#opciones")
+
+
+
+
+
+menuR.click(function(){
+
+    
+    $(opciones).toggle("slow");
+
+
+})
+
+})
